2df94f5e5ef949a3ddbeacf07da62233b6f2b0d3
baldm0mma <jev.forsberg@gmail.com>
2019-10-17 09:36:08 -0600

    Delete default example component and add title, ptag, button, and input components unstyled

--- example/src/App.js
+++ example/src/App.js
@@ -1,13 +1,14 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
-import ExampleComponent from 'my-eighties-computer'
+import { My80sH1, My80sPText } from 'my-eighties-computer';
 
 export default class App extends Component {
-  render () {
+  render() {
     return (
-      <div>
-        <ExampleComponent text='Modern React component module' />
+      <div className='example-container'>
+        <My80sH1 text='You can pass props through this header component' />
+        <My80sPText text='You can pass props through this simple text component' />
       </div>
-    )
+    );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,22 +1,63 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import './index.css';
 
-import styles from './styles.css'
+// H1
+export const My80sH1 = ({ text }) => {
+  return <h1>{text}</h1>;
+};
 
-export default class ExampleComponent extends Component {
-  static propTypes = {
-    text: PropTypes.string
+My80sH1.propTypes = {
+  text: PropTypes.string
+};
+
+// Simple paragraph text
+export const My80sPText = ({ text }) => {
+  return <p>{text}</p>;
+};
+
+My80sPText.propTypes = {
+  text: PropTypes.string
+};
+
+// Input
+export class My80sInput extends Component {
+  constructor() {
+    super();
+    this.state = {
+      content: ''
+    };
   }
 
-  render() {
-    const {
-      text
-    } = this.props
+  handleChange = e => {
+    const content = e.target.value;
+    this.setState({ content });
+  };
 
+  render = () => {
+    const { label, type, placeholder } = this.props;
     return (
-      <div className={styles.test}>
-        Example Component: {text}
-      </div>
-    )
-  }
+      <label>
+        {label}:
+        <input
+          type={type}
+          name={label}
+          placeholder={placeholder}
+          value={this.state.content}
+          onChange={this.handleChange}
+        />
+      </label>
+    );
+  };
 }
+
+My80sInput.propTypes = {
+  label: PropTypes.string,
+  type: PropTypes.string,
+  placeholder: PropTypes.string,
+};
+
+// Button
+export const My80sButton = ({ text, className="", ...props }) => {
+  return <button className={"80s-button " + className} {...props}>{text}</button>;
+};
