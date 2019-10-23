12290a85302359a72f4510eb1c29cb4aef18e388
baldm0mma <jev.forsberg@gmail.com>
2019-10-23 16:01:59 -0600

    Add My80sPText tests

--- src/index.test.js
+++ src/index.test.js
@@ -1,19 +1,31 @@
 import React from 'react';
 import { shallow, mount } from 'enzyme';
-import { My80sH1 } from '.';
+import { My80sH1, My80sPText } from '.';
 
 describe('My80sComponets', () => {
   describe('My80sH1', () => {
-    it('is truthy', () => {
+    it('should be truthy', () => {
       expect(My80sH1).toBeTruthy();
     });
     it('should render without crashing', () => {
       shallow(<My80sH1 />);
     });
     it('should take as an argument, and display, a single text prop', () => {
-      let wrapper = mount(<My80sH1 text='test' />);
-      console.log('props', wrapper.props().text);
-      expect(wrapper.props().text).toEqual('test');
+      let wrapper = mount(<My80sH1 text='h1Test' />);
+      expect(wrapper.props().text).toEqual('h1Test');
+    });
+  });
+
+  describe('My80sPText', () => {
+    it('should be truthy', () => {
+      expect(My80sPText).toBeTruthy();
+    });
+    it('should render without crashing', () => {
+      shallow(<My80sPText />);
+    });
+    it('should take as an argument, and display, a single text prop', () => {
+      let wrapper = mount(<My80sPText text='pTest' />);
+      expect(wrapper.props().text).toEqual('pTest');
     });
   });
 });
